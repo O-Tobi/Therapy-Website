565f334e3d97a9642052617064fe984649b04299
unknown <dunjoyebiz2020@gmail.com>
2024-03-27 14:39:44 +0100

    Finished the therapist intro component

---
+++ src/components/Therapists/Therapist-Intro.js
@@ -0,0 +1,35 @@
+import {Grid, Typography, Box } from "@mui/material";
+
+function TherapistIntro () {
+    return (
+        
+        <Grid container spacing={.5}>
+          <Grid item xs={12} sm={12}  md={4} lg={4}>
+            <Typography variant="h3" sx={{ color: '#008DDA', paddingLeft: {md: '60px', lg:'120px'}, paddingTop: {md: '10px', lg:'10px'} }}>
+              Meet The Therapists
+            </Typography>
+          </Grid>
+      
+        <Grid item xs={12} sm={12} md={1} lg={1}>
+            <Box
+            sx={{
+                backgroundColor: '#FB6D48',
+                width: {xs: '90vw', sm: '90vw', md: '.5vw', lg: '.5vw'},
+                height: {xs: '5px', sm: '5px', md: '140px', lg: '120px'}
+            }}></Box>            
+        </Grid>
+      
+          <Grid item xs={12} sm={12}  md={7} lg={7}>
+            <Typography sx={{
+                paddingRight: {md: '60px', lg:'60px'},
+                paddingTop: {md: '10px', lg:'10px'}
+            }}>
+              Finding the right fit in a therapist is essential to the therapeutic process. Our therapists are trained in: Play Therapy, Equine-Facilitated Psychotherapy, Cognitive Behavioural Therapy, Ecosystemic Structural Family Therapy, Yoga, Triple P Positive Parenting, Gottman Method Couples Therapy and much more!
+            </Typography>
+          </Grid>
+        </Grid>
+        
+    )
+};
+
+export default TherapistIntro;
--- src/components/Therapists/Therapists.js
+++ src/components/Therapists/Therapists.js
@@ -1,31 +1,12 @@
-import { Container, Box, Typography, Grid, Divider} from "@mui/material";
+import { Container } from "@mui/material";
+import TherapistIntro from "./Therapist-Intro";
 
-function Therapists () {
+function Therapists() {
     return (
         <Container>
-            <Grid>
-            <Box>
-                <Typography variant="h3" sx={{
-                    color: '#008DDA'
-                }}>
-                    Meet The Therapists
-                </Typography>
-            </Box>
-            <Divider
-             sx={{
-                height: 10,
-                color: '##008DDA',
-                
-            }}/>
-            <Box>
-                <Typography>
-                    Finding the right fit ina therapist is essential to the therapeutic process. Our therapists are trained in: Play Therapy, Equine-Facilitated Psychotherapy, Cognitive Behavioural Therapy, Ecosystemic Structural Family Therapy, Yoga, Triple P Positive Parenting, Gottman Method Couples Therapy and much more! 
-                </Typography>
-            </Box>
-            </Grid>
-            
+            <TherapistIntro/>
         </Container>
-    )
-};
+    );
+}
 
 export default Therapists;
