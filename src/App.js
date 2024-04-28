9ff98659dd1001e6ab5003a739964af5db9d244e
unknown <dunjoyebiz2020@gmail.com>
2024-04-28 17:48:40 +0100

    Routing the website so other pages will be developed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-
 import './App.css';
 
 import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Service from './components/Company-Service/Service';
-import Therapists from './components/Therapists/Therapists';
-import Guide from './components/Guide/Guide';
+import Home from './components/Home/Home';
 import PatientRight from './components/Patient-right/PatientRight';
 import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header />
-      <Hero />
-      <Service />
-      <Therapists />
-      <Guide />
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
       <PatientRight />
       <Footer />
     </div>
