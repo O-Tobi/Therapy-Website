8020ba74d0e1034758d2bebf5b1e01b2bd7299c7
unknown <dunjoyebiz2020@gmail.com>
2024-06-18 11:12:44 +0100

    reworked the small screen UI for better UX

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,8 +28,79 @@ function Header() {
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
 
+
+                        {/* Nav-links for large screens */}
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                                <Button
+                                    key={page}
+                                    component={NavLink}
+                                    to={page === 'Home' ? '/' : `/${page.toLowerCase()}`}/* {`${page.toLowerCase()}`} */
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'black', display: 'block' }}
+                                >
+                                    {page}
+                                </Button>
+                            ))}
+                        </Box>
+                        
+                        
+                            {/* Logo and company name */}
+                        <PsychologyOutlinedIcon sx={{ fontSize: '3rem', display: 'flex', mr: 1 }} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href="#app-bar-with-responsive-menu"
+                            sx={{
+                                mr: 2,
+                                display: 'flex',
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                                textTransform: 'uppercase',
+                                display: {xs: 'none', sm:'block'}
+
+                            }}
+                        >
+                            {/* ClearMind Therapy Center */}
+                            ctc
+                        </Typography>
+
+                        
+                        
+                        {/* Button */}
+                        <Box >
+                            <Tooltip title='Send Enquiry'>
+                                <Button
+                                    sx={{
+                                        display: { xs: 'none', md: 'flex' },
+                                        color: 'black',
+                                        borderColor: 'black',
+                                    }}
+                                    variant="outlined"
+                                    endIcon={<ArrowRightAltIcon />}
+                                    component={Link}
+                                    to="/contact"
+                                >
+                                    Send Enquiry
+                                </Button>
+
+                            </Tooltip>
+                        </Box>
+
+
                         {/* Hamburger menu for small screens */}
-                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <Box sx={{ 
+                            flexGrow: 1, 
+                            display: { xs: 'flex', md: 'none' },
+                            position: {xs: 'absolute'},
+                            right: { xs: 0 },
+                            
+                            }}>
                             <IconButton
                                 size="large"
                                 aria-label="account of current user"
@@ -61,70 +132,12 @@ function Header() {
                                 {pages.map((page) => (
                                     <MenuItem key={page} onClick={handleCloseNavMenu}>
                                         <Typography textAlign="center">
-                                            <NavLink to= {page === 'Home' ? '/' : `/${page.toLowerCase()}`}>
+                                            <NavLink to={page === 'Home' ? '/' : `/${page.toLowerCase()}`}>
                                                 {page}
                                             </NavLink>
                                         </Typography>
                                     </MenuItem>
                                 ))}
-                            </Menu>
-                        </Box>
-
-                        {/* Nav-links for large screens */}
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            {pages.map((page) => (
-                                <Button
-                                    key={page}
-                                    component={NavLink}
-                                    to={page === 'Home' ? '/' : `/${page.toLowerCase()}`}/* {`${page.toLowerCase()}`} */
-                                    onClick={handleCloseNavMenu}
-                                    sx={{ my: 2, color: 'black', display: 'block' }}
-                                >
-                                    {page}
-                                </Button>
-                            ))}
-                        </Box>
-
-                        {/* Logo and company name */}
-                        <PsychologyOutlinedIcon sx={{ fontSize: '3rem', display: 'flex', mr: 1 }} />
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component="a"
-                            href="#app-bar-with-responsive-menu"
-                            sx={{
-                                mr: 2,
-                                display: 'flex',
-                                flexGrow: 1,
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                                textTransform: 'uppercase',
-
-                            }}
-                        >
-                            {/* ClearMind Therapy Center */}
-                            ctc
-                        </Typography>
-
-                        {/* Button */}
-                        <Box >
-                            <Tooltip title='Send Enquiry'>
-                                <Button
-                                    sx={{
-                                        display: { xs: 'none', md: 'flex' },
-                                        color: 'black',
-                                        borderColor: 'black',
-                                    }}
-                                    variant="outlined" 
-                                    endIcon={<ArrowRightAltIcon />}
-                                    component={Link}
-                                    to="/contact"
-                                    >
-                                    Send Enquiry
-                                </Button>
                                 <IconButton
                                     component={Link}
                                     to="/contact"
@@ -133,9 +146,8 @@ function Header() {
                                     }}>
                                     <ArrowRightAltIcon />
                                 </IconButton>
-                            </Tooltip>
+                            </Menu>
                         </Box>
-
                     </Toolbar>
                 </Container>
             </AppBar>
